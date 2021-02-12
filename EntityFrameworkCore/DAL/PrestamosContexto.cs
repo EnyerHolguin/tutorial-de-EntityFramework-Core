a40eb2b139c9bb0779e7bc6e0f4c66c450933612
Enyer De Jesus Holguin Toribio <enyer_holguin@ucne.edu.do>
2021-02-12 14:18:15 -0400

    Add project files.

---
+++ EntityFrameworkCore/DAL/PrestamosContexto.cs
@@ -0,0 +1,59 @@
+﻿using EntityFrameworkCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCore.DAL
+{
+    public class PrestamosContexto : DbContext
+    {
+        public PrestamosContexto()
+        {
+        }
+
+        public PrestamosContexto(DbContextOptions<PrestamosContexto> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Prestamos> Prestamos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                optionsBuilder.UseSqlite("@” Data Source = Prestamos.db”;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Modern_Spanish_CI_AS");
+
+            modelBuilder.Entity<Prestamos>(entity =>
+            {
+                entity.Property(p => p.Nombres)
+                   .IsRequired()
+                   .HasMaxLength(100)
+                   .IsUnicode(false);
+
+                entity.ToTable("Prestamos");
+
+                 entity.Property(p => p.Apellidos)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+                entity.Property(p => p.Cedula)
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+
+
+                entity.Property(p => p.FechaNacimiento).HasColumnType("date");             
+            });
+
+                     
+        }
+        
+    }
+}
---
+++ EntityFrameworkCore/Models/Prestamos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCore.Models
+{
+    public class Prestamos
+    {
+            public string Nombres { get; set; }
+            public string Apellidos { get; set; }
+            public string Cedula { get; set; }
+            public DateTime FechaNacimiento { get; set; } = DateTime.Now;
+           
+        
+    }
+}
